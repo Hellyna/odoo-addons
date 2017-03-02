aadd15f7179e415291156c26076c85502d44d69d
Hellyna NG <hellyna@groventure.com>
2017-03-02 10:16:17 +0800

    Added base_forms

---
+++ base_forms/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Form Base Utilities",
+    'summary': "Provides useful functions and templates for form building",
+    'description': "Provides useful functions and templates for form building",
+    'version': '0.0.1',
+    'category': '',
+    'website': 'https://github.com/Hellyna/odoo-addons',
+    'author': 'Hellyna NG',
+    'application': False,
+    'installable': True,
+    'license': 'MIT',
+    'depends': [
+    ],
+    'data': [
+        'views/main/select.xml',
+    ],
+    'test': [
+    ],
+}
